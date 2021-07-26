b960745319135e4181c748880c28e583c9ba043e
deceir <83772817+deceir@users.noreply.github.com>
2021-07-26 11:15:59 -0500

    additional setup

--- js/movies.js
+++ js/movies.js
@@ -1,10 +1,6 @@
 "use strict"
 
-const url = https://silent-low-kicker.glitch.me/movies/
+const url = `https://silent-low-kicker.glitch.me/movies/`
 
-const loadingDataSpinner =
-    let html = <div className="spinner-border text-dark" role="status"><span className="sr-only"><p>Loading...</p></span></div>
 
-function startLoadSpinner() {
-    $()
-}
+
